e21d1b76c115c1108ffe3627355e09212f9349ca
DESKTOP-79U09O2\Felix Livshits <a@b.com>
2017-07-10 10:34:29 -0400

    changing names for build triggers.

--- conf.js
+++ conf.js
@@ -1,6 +1,6 @@
 exports.config = {
     seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
-    specs: ['homePage.js'],
+    specs: ['CanTireDemo.js'],
     capabilities: {
         browserName: 'chrome'
       },
