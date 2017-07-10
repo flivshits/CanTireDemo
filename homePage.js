72dca11683a487d30fedc0e5b7401f50c06bdd28
DESKTOP-79U09O2\Felix Livshits <a@b.com>
2017-07-10 09:51:21 -0400

    small change to test polling.

--- homePage.js
+++ homePage.js
@@ -33,7 +33,7 @@ describe('Given that I go to istockphoto', function() {
 			landingPage.signIn.click();
 			expect(loginPage.signInLabel.isPresent()).toBe(true);
 			browser.sleep(2500);
-			loginPage.signInAccount.sendKeys("pray this works");
+			loginPage.signInAccount.sendKeys("Canadian Tire");
 			browser.sleep(2500);
 			loginPage.signInPassword.sendKeys("this should also work");
 			browser.sleep(2500);
@@ -63,7 +63,7 @@ describe('Given that I go to istockphoto', function() {
 			landingPage.signIn.click();
 			expect(loginPage.signInLabel.isPresent()).toBe(false);
 			browser.sleep(2500);
-			loginPage.signInAccount.sendKeys("pray this works");
+			loginPage.signInAccount.sendKeys("Canadian Tire");
 			browser.sleep(2500);
 			loginPage.signInPassword.sendKeys("this should also work");
 			browser.sleep(2500);
